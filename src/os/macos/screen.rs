da68e8c8d891181d2d4c0d5b5cb1f2d0fb242b32
Nikolai Vazquez <nvazquez1297@gmail.com>
2018-03-18 23:28:49 -0400

    Add screen::online_displays

--- src/os/macos/screen.rs
+++ src/os/macos/screen.rs
@@ -2,7 +2,15 @@
 
 extern {
     fn CGMainDisplayID() -> DisplayId;
+
+    fn CGGetOnlineDisplayList(
+        max_displays: u32,
+        online_displays: *mut DisplayId,
+        displayCount: *mut u32
+    ) -> CGError;
 }
+
+type CGError = i32;
 
 /// Returns the ID of the main display.
 ///
@@ -19,6 +27,34 @@ extern {
 #[inline]
 pub fn main_display() -> DisplayId {
     unsafe { CGMainDisplayID() }
+}
+
+/// Returns IDs for displays that are online (active, mirrored, or sleeping).
+///
+/// If the framebuffer hardware is connected, a display is considered connected
+/// or online.
+///
+/// When hardware mirroring is used, a display can be online but not active or
+/// drawable. Programs that manipulate display settings (such as gamma tables)
+/// need access to all displays, including hardware mirrors, which are not
+/// drawable.
+#[inline]
+pub fn online_displays() -> Option<Vec<DisplayId>> {
+    macro_rules! handle {
+        ($e:expr) => { match $e {
+            0 => (),
+            _ => return None,
+        } }
+    }
+
+    let mut count = 0u32;
+    handle!(unsafe { CGGetOnlineDisplayList(0, 0 as _, &mut count) });
+
+    let mut buffer = vec![0; count as usize];
+    let ptr = buffer.as_mut_ptr();
+    handle!(unsafe { CGGetOnlineDisplayList(count, ptr, &mut count) });
+
+    Some(buffer)
 }
 
 /// A unique identifier for an attached display.
