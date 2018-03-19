b67e17afc9b0cb3dc00fc210cf49666c8c8235d4
Nikolai Vazquez <nvazquez1297@gmail.com>
2018-03-18 23:06:28 -0400

    Add docs to DisplayId::main_display

--- src/os/macos/screen.rs
+++ src/os/macos/screen.rs
@@ -5,6 +5,17 @@ extern {
 }
 
 /// Returns the ID of the main display.
+///
+/// The main display is the display with its screen location at (0,0) in the
+/// global display coordinate space. In a system without display mirroring, the
+/// display with the menu bar is typically the main display.
+///
+/// If mirroring is enabled and the menu bar appears on more than one display,
+/// this function provides a reliable way to find the main display.
+///
+/// In case of hardware mirroring, the drawable display becomes the main
+/// display. In case of software mirroring, the display with the highest
+/// resolution and deepest pixel depth typically becomes the main display.
 #[inline]
 pub fn main_display() -> DisplayId {
     unsafe { CGMainDisplayID() }
