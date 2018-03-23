94e25dc1bdc08960ca9c7ab76edfd7dda8556641
Nikolai Vazquez <nvazquez1297@gmail.com>
2018-03-23 09:22:49 -0400

    Fix docs for color::Rgb

--- src/color.rs
+++ src/color.rs
@@ -3,10 +3,10 @@
 /// A simple RGB color.
 #[derive(Copy, Clone, Debug, PartialEq)]
 pub struct Rgb {
-    /// The amount of red, ranging from 0.0 to 1.0.
+    /// The amount of red, ranging from 0 to 255.
     pub red: u8,
-    /// The amount of green, ranging from 0.0 to 1.0.
+    /// The amount of green, ranging from 0 to 255.
     pub green: u8,
-    /// The amount of green, ranging from 0.0 to 1.0.
+    /// The amount of green, ranging from 0 to 255.
     pub blue: u8,
 }
