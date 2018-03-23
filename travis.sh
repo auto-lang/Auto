696569eca28a7c7184ad0d06c87bd30167850e50
Nikolai Vazquez <nvazquez1297@gmail.com>
2018-03-23 13:41:08 -0400

    [Travis] Install clippy during different phase
    Change: will now fail if clippy fails to compile.

--- travis.sh
+++ travis.sh
@@ -3,10 +3,6 @@
 set -e
 
 if [[ -n "$CLIPPY" ]]; then
-    if ! cargo install clippy --debug --force; then
-        echo "COULD NOT COMPILE CLIPPY, IGNORING CLIPPY TESTS"
-        exit
-    fi
     cargo clippy -- -Dclippy
 else
     cargo test
